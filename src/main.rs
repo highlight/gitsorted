a8b63d5089549411ea6fc4af9d48a1baa4aa0095
Jay Khatri <jaykhatrimail@gmail.com>
2023-11-05 14:08:19 -0800

    making some progress

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use serde::{Deserialize, Serialize};
 
 use chrono::{DateTime, Utc};
 use postgrest::Postgrest;
+use serde_json::json;
 use std::time::Duration;
 
+use reqwest;
 use rocket::{
     tokio::{task, time},
     Request, State,
@@ -22,6 +24,8 @@ struct IssueObject {
     number: i32,
     created_at: String,
     title: String,
+    last_processed: String,
+    author: String,
 }
 // simple health check endpoint on /healthz
 #[get("/healthz")]
@@ -108,7 +112,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn job_func() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO, we shouldn't initialize supabase twice here. 
+    // TODO, we shouldn't initialize supabase twice here.
     let pg2 = Postgrest::new(
         option_env!("SUPABASE_URL")
             .ok_or("error finding supabase url")?
@@ -120,11 +124,14 @@ async fn job_func() -> Result<(), Box<dyn std::error::Error>> {
             .ok_or("error finding supabase token")?
             .to_string(),
     );
-    // retrieve the most recent record in the issues db.
+    // retrieve the most recently updated record in the issues db.
+    // this represents the last time we did a scan of the issues.
+    let now = Utc::now();
     let res = pg2
         .from("Issues")
         .select("*")
-        .order("created_at.desc")
+        .not("is", "last_processed", "null")
+        .order("last_processed.desc")
         .limit(1)
         .execute()
         .await?
@@ -136,8 +143,11 @@ async fn job_func() -> Result<(), Box<dyn std::error::Error>> {
     if obj.len() != 1 {
         return Err("length of return value from supabase is not 1".into());
     }
-    let datetime: DateTime<Utc> = obj[0].created_at.parse().map_err(|e| format!("error parsing datetime: {}", e))?;
-    log::info!("latest datetime is: {}", datetime);
+    let last_processed_datetime: DateTime<Utc> = obj[0]
+        .last_processed
+        .parse()
+        .map_err(|e| format!("error parsing datetime: {}", e))?;
+    log::info!("last processed datetime is '{}' for issue #{} with title '{}'", last_processed_datetime, obj[0].number, obj[0].title);
     let mut issue_vec: Vec<IssueObject> = Vec::new();
     let mut page = octocrab::instance()
         .issues("highlight", "highlight")
@@ -146,16 +156,27 @@ async fn job_func() -> Result<(), Box<dyn std::error::Error>> {
         .per_page(50)
         .send()
         .await?;
+    // TODO: add "jay-khatri" once we know this is working.
+    let highlight_devs: Vec<&str> = vec![
+        "Vadman97",
+        "SpennyNDaJets",
+        "deltaepsilon",
+        "ccschmitz",
+        "mayberryzane",
+    ];
     loop {
         for issue in &page {
-            if issue.created_at <= datetime {
+            if issue.created_at < last_processed_datetime {
                 break;
             }
+
             issue_vec.push(IssueObject {
                 number: issue.number.to_string().parse::<i32>()?,
                 id: issue.id.to_string().parse::<i32>()?,
                 created_at: issue.created_at.to_string(),
                 title: issue.title.to_string(),
+                last_processed: Utc::now().to_string(),
+                author: issue.user.login.to_string(),
             });
         }
         page = match octocrab::instance()
@@ -166,15 +187,57 @@ async fn job_func() -> Result<(), Box<dyn std::error::Error>> {
             None => break,
         }
     }
-    log::info!("Found {:?} new issues to write", issue_vec.len());
+
+    log::info!("Found {:?} new issues to write", &issue_vec.len());
+
+    // post in slack if we find an brand new issue
+    let webhook_url =
+        "https://hooks.slack.com/services/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";
+
+    let client = reqwest::Client::new();
+    // enumerate this for loop , i want an index for each entry
+    for issue in &mut issue_vec.iter_mut() {
+        // make sure that all issues that we processed have their "last_processed" time updated.
+        issue.last_processed = now.to_string();
+        // filter our issues created by a certain person.
+        // convert string to str
+        if highlight_devs.contains(&issue.author.as_str()) {
+            log::info!("Ignoring {:?} because its author is at Highlight", issue.number);
+            break;
+        }
+        log::info!("Not ignoring {:?} because its author is external", issue.number);
+
+        let new_issues_message = json!({
+            "text": format!("
+            I noticed a new external <https://github.com/highlight/highlight/issues/{}|issue>. Take a look?", issue.number),
+        });
+        let res = client
+            .post(webhook_url)
+            .body(new_issues_message.to_string())
+            .send()
+            .await?;
+        if res.status().is_success() {
+            log::info!("sent message to slack for issue: {}", issue.number);
+        } else {
+            log::error!("error sending message to slack for issue: {}", issue.number);
+        }
+        // post on the issue to notify the the user that we've seen it.
+        octocrab::instance().issues("highlight", "highlight")
+                .create_comment(issue.number.try_into()?, "Hi, this is Highlight.io's [GitSorted](https://gitsorted.onrender.com/) bot. 
+                While one of our developers cooks up a reply, please search for anything related in our [Discord](https://highlight.io/community) or on our [docs](https://highlight.io/docs). 
+                Also, if you haven't posted a reproduction, please do so (we prioritize those tickets).")
+                .await.map_err(|e| format!("error writing comment to GitHub: {}", e))?;
+        log::info!("sent reply  on github for issue: {}", issue.number);
+    }
+
     if issue_vec.len() != 0 {
-        pg2.from("Issues")
-            .insert(serde_json::to_string(&issue_vec)?)
+        let resp = pg2.from("Issues")
+            .upsert(serde_json::to_string(&issue_vec)?)
             .execute()
             .await?
             .text()
             .await?;
+        log::info!("attempted writing {} issues to the db. response: \n{}", issue_vec.len(), resp.as_str())
     }
     Ok(())
 }
-
